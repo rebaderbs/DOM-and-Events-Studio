3cac5bbc4dd57a44ac854b090c44567b328aed10
Rebecca Derbak <rderbak@rebeccas-mbp.lan>
2022-06-28 08:43:41 -0500

    added events for up, down, left, and right; also did half of the bonus mission where it returns shuttle to original position

--- scripts.js
+++ scripts.js
@@ -5,7 +5,14 @@ function init() {
     const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
     const landing = document.getElementById("landing");
     const missionAbort = document.getElementById("missionAbort");
-
+    const up = document.getElementById("up");
+    const down = document.getElementById("down");
+    const right = document.getElementById("right");
+    const left = document.getElementById("left");
+    const rocket = document.getElementById("rocket");
+    rocket.style.position = "absolute";
+    rocket.style.left = "325px";
+    rocket.style.bottom = "0px";
 
     takeoff.addEventListener('click', function()    {
         let result = window.confirm("Confirm that the shuttle is ready for takeoff.");
@@ -13,7 +20,9 @@ function init() {
             flightStatus.innerHTML = "Shuttle in flight."
             shuttleBackground.style.backgroundColor = "blue";
             spaceShuttleHeight.innerHTML = "10000";
-
+            //bonus mission
+            move = parseInt(rocket.style.bottom) + 10 + "px";
+            rocket.style.bottom = move;
         }
     });
 
@@ -23,6 +32,9 @@ function init() {
             flightStatus.innerHTML = "The shuttle has landed.";
             shuttleBackground.style.backgroundColor = "green";
             spaceShuttleHeight.innerHTML = "0";
+            //bonus mission
+            rocket.style.bottom = "0px";
+            rocket.style.left = "325px";
         }
     });
 
@@ -32,31 +44,33 @@ function init() {
             flightStatus.innerHTML = "Mission aborted.";
             shuttleBackground.style.backgroundColor = "green";
             spaceShuttleHeight.innerHTML = "0";
+            //bonus mission
+            rocket.style.bottom = "0px";
+            rocket.style.left = "325px";
         }
+    });
+
+    up.addEventListener('click', function() {
+        move = parseInt(rocket.style.bottom) + 10 + "px";
+        rocket.style.bottom = move;
+        spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) + 10000;
+    });
+
+    down.addEventListener('click', function()   {
+        move = parseInt(rocket.style.bottom) - 10 + "px";
+        rocket.style.bottom = move;
+        spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) - 10000;
+    });
+
+    right.addEventListener('click', function()  {
+        move = parseInt(rocket.style.left) + 10 + "px";
+        rocket.style.left = move;
+    });
+
+    left.addEventListener('click', function()   {
+        move = parseInt(rocket.style.left) - 10 + "px";
+        rocket.style.left = move;
     });
 }
 
 window.addEventListener('load', init);
-
-
-// 1. When the "Take off" button is clicked, the following should happen:
-//      A window confirm should let the user know "Confirm that the shuttle is ready for takeoff." If the shuttle is ready for liftoff, then add parts b-d.
-//      The flight status should change to "Shuttle in flight." flightstatus is id!!!!
-//      The background color of the shuttle flight screen (id = "shuttleBackground") should change from green to blue.
-//      The shuttle height should increase by 10,000 miles.
-
-// 2. When the "Land" button is clicked, the following should happen:
-//      A window alert should let the user know "The shuttle is landing. Landing gear engaged."
-//      The flight status should change to "The shuttle has landed."
-//      The background color of the shuttle flight screen should change from blue to green.
-//      The shuttle height should go down to 0.
-
-// 3. When the "Abort Mission" button is clicked, the following should happen:
-//       window confirm should let the user know "Confirm that you want to abort the mission." If the user wants to abort the mission, then add parts b-d.
-//      The flight status should change to "Mission aborted."
-//      The background color of the shuttle flight screen should change from blue to green.
-//      The shuttle height should go to 0.
-
-// 4. When the "Up", "Down", "Right", and "Left" buttons are clicked, the following should happen:
-//      he rocket image should move 10 px in the direction of the button that was clicked.
-//      If the "Up" or "Down" buttons were clicked, then the shuttle height should increase or decrease by 10,000 miles.
